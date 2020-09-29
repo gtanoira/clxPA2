2fede0ca981972e2fe2fc927415246655ffbc36c
Gonzalo Tanoira <gonzalo.mtanoira@gmail.com>
2020-09-29 20:12:58 -0300

    Minor changes

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -5,6 +5,6 @@ export const environment = {
     sapGwServer: 'http://clxsapjgw02:8080/ClxWebService',
     loginServer: 'http://logincentraldev.claxson.com',
     portalAdminServer: 'http://portaladmindev.claxson.com',
-    hotgoBackendServer: 'http://hgdw.claxson.com'
+    hotgoBackendServer: 'http://admapps02:5000'
   }
 };
