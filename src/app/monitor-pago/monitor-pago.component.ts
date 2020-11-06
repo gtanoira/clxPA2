3bedcd7f75a6c148e9b7b877dce563631951a6fd
Gonzalo Tanoira <gonzalo.mtanoira@gmail.com>
2020-11-05 23:43:01 -0300

    Adds a Monitor de pagos, mostrar facturas

--- src/app/monitor-pago/monitor-pago.component.ts
+++ src/app/monitor-pago/monitor-pago.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import * as moment from 'moment';
 
 // Services
@@ -15,22 +19,35 @@ import { NuevoPagoModalComponent } from './nuevo-pago/nuevo-pago.component';
 @Component({
   selector: 'app-monitor-pago',
   templateUrl: './monitor-pago.component.html',
-  styleUrls: ['./monitor-pago.component.scss']
+  styleUrls: ['./monitor-pago.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style( {height: '0px', minHeight: '0'} )),
+      state('expanded', style( {height: '*'} )),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class MonitorPagoComponent implements OnInit {
+export class MonitorPagoComponent implements OnInit, AfterViewInit {
 
   // Variables
   public estadoPago = 'I'; // indicará el estado en que se encuentra el monitor de pago: (I)inicial / (S)simular
   public nuevoPagoModal = false;
 
   // Tabla de facturas a pagar (partidas)
-  public partidas: PagoPartidaModel[] = [];
-  public columnsToDisplay = ['proveedorDesc', 'vtoFecha', 'docNro', 'docMoneda', 'docImporte'];
+  // public partidas: PagoPartidaModel[] = [];
+  public columnsToDisplay = ['select', 'expandIcon', 'proveedorDesc', 'vtoFecha', 'docNro', 'docMoneda', 'docImporte'];
+  public dataSource = new MatTableDataSource<PagoPartidaModel>([]);
+  public expandedElement: PagoPartidaModel | null;  // Indica que row está expandido
+  public selection = new SelectionModel<PagoPartidaModel>(true, []);
 
   // Varaibles para filtrar las partidas del Nuevo Pago
   private empresaId = '';
   private fechaPago: moment.Moment;
   private viaPago = '';
+
+  // HTML element para ordenar la tabla (sort)
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private errorMessageService: ErrorMessageService,
@@ -39,6 +56,10 @@ export class MonitorPagoComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
   // Solicitar los datos para crear la nueva solicitud de pago
   public nuevoPagoDialog(): void {
@@ -67,8 +88,7 @@ export class MonitorPagoComponent implements OnInit {
               if (partidas.length <= 0) {
                 this.errorMessageService.changeErrorMessage('No se han encontrado facturas que pagar.');
               } else {
-                console.log('*** PARTIDAS:', partidas);
-                this.partidas = partidas;
+                this.dataSource.data = partidas;
               }
             },
             error => {
@@ -78,6 +98,20 @@ export class MonitorPagoComponent implements OnInit {
         }
       }
     );
+  }
+
+  // Chequear si se han seleccionado todas las facturas
+  public isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /// Seleccionar o deseleccionar todas las columnas
+  public masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
   }
 
   // Simular pago
--- src/app/monitor-pago/monitor-pago.module.ts
+++ src/app/monitor-pago/monitor-pago.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ReactiveFormsModule } from '@angular/forms';
 
 // Modules
@@ -31,13 +34,16 @@ import { SapService } from '../shared/sap.service';
     CommonModule,
     // Angular Material
     MatButtonModule,
+    MatCheckboxModule,
     MatDatepickerModule,
     MatDialogModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatSelectModule,
+    MatSortModule,
     MatTableModule,
+    MatTooltipModule,
     ReactiveFormsModule
   ],
   providers: [
